e681a8aba5ca40b026e2121a5d18d7c918089bc0
stoneYuan3 <jackyuan110@gmail.com>
2022-12-13 12:36:55 -0800

    finalize work detail page layout, fill in risk map project detail, responsive layout feature development, add external link 111-12-13-01

--- js/work.js
+++ js/work.js
@@ -8,8 +8,8 @@
   var link_gallery=document.getElementById('link-gallery');
   link_gallery.classList.remove('style-selected');
 
-  if(workName=='riskmap'){
-    var title=document.getElementsByTagName('h1')[0];
-    title.innerHTML='529 Garage Risk Map';
-    // $('h1').html('529 Garage Risk Map');
-  }
+  // if(workName=='riskmap'){
+  //   var title=document.getElementsByTagName('h1')[0];
+  //   title.innerHTML='529 Garage Risk Map';
+  //   // $('h1').html('529 Garage Risk Map');
+  // }
